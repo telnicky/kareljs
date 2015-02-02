22d32810e4b335cedf1a48f8ce04ae18a370d30d
Travis Elnicky <telnicky@instructure.com>
2015-02-02 09:45:17 -0700

    first cut at queing commands

--- game_object.js
+++ game_object.js
@@ -1,7 +1,7 @@
 var GameObject = {
   running: false,
-  currentLine: null,
   lines: null,
+  updateQueue: [],
 
   initialize: function(level) {
     this.level = level;
@@ -13,6 +13,26 @@ var GameObject = {
     $('.run').click(this.onRun.bind(this));
     this.main();
     setInterval(this.update.bind(this), 800);
+  },
+
+  commands: {
+    move: function(obj) {
+      if (obj.world.canMove(obj.karel.direction, obj.karel.x, obj.karel.y)) {
+        obj.karel.move();
+      }
+    },
+
+    turnLeft: function(obj) {
+      obj.karel.turnLeft();
+    },
+
+    putBeeper: function(obj) {
+      obj.world.putBeeper(obj.karel.x, obj.karel.y);
+    },
+
+    pickBeeper: function(obj) {
+      obj.world.pickBeeper(obj.karel.x, obj.karel.y);
+    }
   },
 
   code: function() {
@@ -36,30 +56,37 @@ var GameObject = {
     }
 
     this.running = true;
-    this.lines = this.code().split('\n');
-    this.currentLine = this.lines.shift();
   },
 
-  run: function(line) {
-    var move = function() {
-      if (this.world.canMove(this.karel.direction, this.karel.x, this.karel.y)) {
-        this.karel.move();
-      }
-    }.bind(this);
+  parseLines: function(code) {
+    var lines = code.split('\n');
+  },
 
-    var turnLeft = function() { this.karel.turnLeft(); }.bind(this);
-    var putBeeper = function() { this.world.putBeeper(this.karel.x, this.karel.y); }.bind(this);
-    var pickBeeper = function() { this.world.pickBeeper(this.karel.x, this.karel.y); }.bind(this);
+  queueCommand: function(command) {
+    this.updateQueue.push(command);
+  },
 
-    eval(line);
-    this.currentLine = this.lines.shift();
+  run: function() {
+    var move = function() { this.queueCommand("move"); }.bind(this);
+    var turnLeft = function() { this.queueCommand("turnLeft"); }.bind(this);
+    var putBeeper = function() { this.queueCommand("putBeeper"); }.bind(this);
+    var pickBeeper = function() { this.queueCommand("pickBeeper"); }.bind(this);
+
+    eval(this.code());
   },
 
   update: function() {
-    if (this.running && this.currentLine) {
-      this.run(this.currentLine);
+    console.log(this.updateQueue);
+    if (this.running) {
+      this.run();
+      this.running = false;
     } else {
       this.running = false;
+    }
+
+    if (this.updateQueue.length > 0) {
+      var command = this.updateQueue.pop();
+      this.commands[command](this);
     }
   }
 };
